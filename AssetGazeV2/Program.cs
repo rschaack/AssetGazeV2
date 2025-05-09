7fe24357c1a1cc22a64536c0fc7ca5ed95864d54
schaacktwilltac <rolandschaack@twilltac.com>
2025-05-09 10:52:05 +0100

    First commit setting up console.

---
+++ AssetGazeV2/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AssetGazeV2;
+
+class Program
+{
+
+    
+    static void Main(string[] args)
+    {
+        var godService = new GodService(false);
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n--- Main Menu ---");
+            Console.WriteLine("1. Run application");
+            Console.WriteLine("q. Exit");
+            Console.Write("Enter your choice: \n");
+
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    godService.HandleOptionOne();
+                    break;
+                case "q": // Allow 'q' or 'Q' as an alternative exit
+                case "Q":
+                    running = false;
+                    Console.WriteLine("Exiting application...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Application closed.");
+    }
+
+  
+}
+
+public class GodService
+{
+    public GodService(bool isTestMode)
+    {
+    }
+
+    public void HandleOptionOne()
+    {
+        Console.WriteLine("Do some stuff.");
+        Console.WriteLine("Done some stuff.");
+    }
+}
+
+
+
+
+
